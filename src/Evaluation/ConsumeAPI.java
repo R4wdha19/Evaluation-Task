1080faf218f40fbdcb539de29e7b7ced5e14d90c
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2022-11-29 12:47:09 +0400

    Missed up everything

--- src/Evaluation/ConsumeAPI.java
+++ src/Evaluation/ConsumeAPI.java
@@ -1,5 +1,8 @@
 package Evaluation;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,6 +11,7 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 // consuming The Given Api , Getting The results From it . 
@@ -28,36 +32,53 @@ public class ConsumeAPI {
 		Path myFile = Path.of("C:\\Users\\user013\\Desktop\\Test.txt");
 		Files.writeString(myFile, response.body(), StandardCharsets.UTF_8);
 		boolean reading = true;
+
 		// Searching from a file for one word
 		while (reading) {
 			Path pathToFile = Path.of("C:\\Users\\user013\\Desktop\\Test.txt");
 
 			System.out.println(" Enter The Word That You Want To Search for ");
 			String words = sc.nextLine();
-			String stringFromFile = "";
 
+			String stringFromFile = "";
+			File file1 = new File("C:\\Users\\user013\\Desktop\\Test.txt");
+			ArrayList wordsList = new ArrayList();
+			wordsList.add(words);
+			FileReader fileReaderObj = new FileReader(file1);
+			BufferedReader bufferReaderObj = new BufferedReader(fileReaderObj);
+			String search;
+
+			int count = 0;
 			stringFromFile = Files.readString(pathToFile);
 
 			try {
+
 				stringFromFile = Files.readString(pathToFile);
-				// Error handling
-			} catch (IOException e) {
+
+				if (wordsList.equals(words))
+
+				{
+					count++;
+				}
+
+				if (count != 0 && stringFromFile.contains(words)) {
+
+					System.out.println(words + "Word is Found" + count + "Times ");
+
+				} else {
+					System.out.println("Word is Not Found");
+				}
+
+				fileReaderObj.close();
+			}
+			// Error handling
+			catch (IOException e) {
 				System.out.println(e.getMessage());
 
 			}
 
-			if (stringFromFile.contains(words)) {
-
-				System.out.println(words + " Is Found");
-			} else {
-				System.out.println(" Word Not Found");
-			}
-
 		}
 		reading = false;
-
-		// Searching for words from file
-		String[] wordsList;
 
 	}
 }
