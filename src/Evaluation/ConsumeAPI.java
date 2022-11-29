0a6ad5385362883e01c048fb807c1a9f47955c5d
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2022-11-29 10:10:01 +0400

    Writing To file

--- src/Evaluation/ConsumeAPI.java
+++ src/Evaluation/ConsumeAPI.java
@@ -5,6 +5,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 // consuming The Given Api , Getting The results From it . 
 
@@ -19,18 +22,10 @@ public class ConsumeAPI {
 
 		System.out.println(response.body());
 
+		// Writing it to file
+		Path myFile = Path.of("C:\\Users\\user013\\Desktop\\Test.txt");
+		Files.writeString(myFile, response.body(), StandardCharsets.UTF_8);
+
 	}
 
-	public class ConsumeAPI {
-		public static void main(String[] args) throws IOException, InterruptedException {
-
-			HttpClient client = HttpClient.newHttpClient();
-			HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.nationalize.io/?name=nathaniel"))
-					.GET().build();
-
-			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-			System.out.println(response.body());
-
-		}
 }
