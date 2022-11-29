d5e0f31d8150741a6c5960d794956b1e47dfbb4f
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2022-11-29 10:06:43 +0400

    Testing again

--- src/Evaluation/ConsumeAPI.java
+++ src/Evaluation/ConsumeAPI.java
@@ -19,5 +19,18 @@ public class ConsumeAPI {
 
 		System.out.println(response.body());
 
-	}yghtgyyt
+	}
+
+	public class ConsumeAPI {
+		public static void main(String[] args) throws IOException, InterruptedException {
+
+			HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.nationalize.io/?name=nathaniel"))
+					.GET().build();
+
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+			System.out.println(response.body());
+
+		}
 }
