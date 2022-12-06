a7764e3c986f47f54884e1669fb8b15144173821
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2022-12-06 11:56:57 +0400

    Added options to Search From

--- src/Evaluation/ConsumeAPI.java
+++ src/Evaluation/ConsumeAPI.java
@@ -9,6 +9,10 @@ public class ConsumeAPI {
 	public static void main(String[] args) throws IOException, InterruptedException {
 
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Please Choose An Option : ");
+		System.out.println(" 1 : Writting Response To File  ");
+		System.out.println(" 2 : Search For A Word  ");
+		System.out.println(" 3 : Find Duplicated And Unique Words ");
 		int option = sc.nextInt();
 		switch (option) {
 		case 1:
@@ -18,10 +22,20 @@ public class ConsumeAPI {
 		case 2:
 			System.out.println("Where Do You Want TO Search From ? PDF OR TEXT FILE . ");
 			System.out.println("If You Want To Search From Text File press 1 ");
+			System.out.println("If You Want To Search From PDF File press 2 ");
+			int pChoices = sc.nextInt();
 
-			System.out.println("Search For A Word ");
-			SearchForWord.SearchForOneWord();
-			break;
+			switch (pChoices) {
+			case 1:
+				System.out.println("Search For A Word From Text File ");
+				SearchForWord.SearchForOneWord();
+				break;
+			case 2:
+				System.out.println("Search For A Word From PDF File ");
+
+				break;
+			}
+
 		case 3:
 			System.out.println("Duplicated And Unique Words ");
 			DuplicatedWords.SearchForDuplicatedWords();
